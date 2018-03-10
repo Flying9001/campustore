fff7c4472816bcb9cbc524df097e2546274ec462
flying9001 <flying9001@gmail.com>
2018-03-10 12:18:28 +0800

    add userController and test successfully

--- src/main/java/com/ljq/cs/web/controller/UserController.java
+++ src/main/java/com/ljq/cs/web/controller/UserController.java
@@ -1,12 +1,48 @@
 package com.ljq.cs.web.controller;
 
+import com.ljq.cs.entity.UserInfo;
+import com.ljq.cs.service.UserInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Desccription: 用户操作控制中心
  * @author: Thaumaturge
  * @email: lujunqiang@ancun.com
  * @date: 2018/3/8
  */
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    private static final Logger logback = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+
+    @RequestMapping(value = "queryOneInfo")
+    private String queryOneInfo(UserInfo userInfo, Model model){
+
+
+        if(userInfo == null){
+            logback.debug("request userInfo param is null !");
+            return "redirect: /homepage";
+        }
+        logback.debug(userInfo.toString());
+        userInfo = userInfoService.queryOneInfo(userInfo);
+        if(userInfo == null){
+            return "forward:/homepage";
+        }
+        logback.debug(userInfo.toString());
+        model.addAttribute("userInfo",userInfo);
+        return "homepage";
+
+    }
 
 
 
--- src/main/webapp/resources/view/user/usersignin.jsp
+++ src/main/webapp/resources/view/user/usersignin.jsp
@@ -4,7 +4,6 @@
 	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + 
 		request.getServerPort() + path + "/";	
 %>
-<%@taglib uri="/struts-tags" prefix="s" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
